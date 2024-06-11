a6f58f9e90fe7ce5fde1681face50f23b88845f8
Juan Carlos <juancarlosmontanod@gmail.com>
2024-06-10 21:39:55 -0700

    supabase enviromental variables

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,6 +2,8 @@ import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  'https://dqdlecrcfnmridxmrolx.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxZGxlY3JjZm5tcmlkeG1yb2x4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc1NTg0MTEsImV4cCI6MjAzMzEzNDQxMX0.kg1FRGSDAqSprDWRDwgKv64uzYR_kKNlrDdruuvgMRk'
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
 )
+
+console.log(import.meta.env.VITE_SUPER_SECRET_KEY)
